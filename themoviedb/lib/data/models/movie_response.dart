8105ed50c60cd298fbd07ebc77c3a428f6783cc1
petro.ubohyi <petro.ubohyi@globallogic.com>
2022-01-19 14:11:18 +0200

    created MovieResponse class and generated movie.g.dart, movie_response.g.dart for JSON parsing

---
+++ themoviedb/lib/data/models/movie_response.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'movie.dart';
+
+part 'movie_response.g.dart';
+
+@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+class MovieResponse {
+  final int page;
+  @JsonKey(name: 'results')
+  final List<Movie> movies;
+  final int totalResults;
+  final int totalPages;
+
+  MovieResponse(
+      {required this.page,
+      required this.movies,
+      required this.totalResults,
+      required this.totalPages});
+
+  factory MovieResponse.fromJson(Map<String, dynamic> json) =>
+      _$MovieResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MovieResponseToJson(this);
+}
---
+++ themoviedb/lib/data/models/movie_response.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'movie_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MovieResponse _$MovieResponseFromJson(Map<String, dynamic> json) =>
+    MovieResponse(
+      page: json['page'] as int,
+      movies: (json['results'] as List<dynamic>)
+          .map((e) => Movie.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      totalResults: json['total_results'] as int,
+      totalPages: json['total_pages'] as int,
+    );
+
+Map<String, dynamic> _$MovieResponseToJson(MovieResponse instance) =>
+    <String, dynamic>{
+      'page': instance.page,
+      'results': instance.movies.map((e) => e.toJson()).toList(),
+      'total_results': instance.totalResults,
+      'total_pages': instance.totalPages,
+    };
