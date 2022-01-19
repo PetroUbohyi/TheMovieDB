1db18504b9c2a680bdae1ed0a83ceaa69d86e8d0
petro.ubohyi <petro.ubohyi@globallogic.com>
2022-01-19 12:38:37 +0200

    create Movie class for work with popular movie API

--- themoviedb/lib/data/models/movie.dart
+++ themoviedb/lib/data/models/movie.dart
@@ -1,10 +1,32 @@
 class Movie {
+  final String? posterPath;
+  final bool adult;
+  final String overview;
+  final DateTime? releaseDate;
+  final List<int> genre_ids;
+  final int id;
+  final String originalTitle;
+  final String originalLanguage;
   final String title;
-  final String description;
-  final DateTime releaseDate;
+  final String? backdropPath;
+  final double popularity;
+  final int voteCount;
+  final bool video;
+  final double voteAverage;
 
   Movie(
-      {required this.title,
-      required this.description,
-      required this.releaseDate});
+      {required this.posterPath,
+      required this.adult,
+      required this.overview,
+      required this.releaseDate,
+      required this.genre_ids,
+      required this.id,
+      required this.originalTitle,
+      required this.originalLanguage,
+      required this.title,
+      required this.backdropPath,
+      required this.popularity,
+      required this.voteCount,
+      required this.video,
+      required this.voteAverage});
 }
