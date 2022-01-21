fceb2529f46d4b65bb368a59d5911a024e0d3b9d
petro.ubohyi <petro.ubohyi@globallogic.com>
2022-01-21 16:55:52 +0200

    API is connected to the movie list

--- themoviedb/lib/data/repository.dart
+++ themoviedb/lib/data/repository.dart
@@ -1,3 +1,17 @@
+import 'package:themoviedb/data/api_client.dart';
+import 'package:themoviedb/data/models/movie.dart';
+
 class Repository {
-  
+  final ApiClient apiClient;
+
+  Repository({required this.apiClient});
+
+  Future<List<Movie>> fetchMovie() async {
+    final movieList = apiClient.topRatedMovies();
+    print('Movie LIST: $movieList');
+    return movieList;
+  }
+
+
+
 }
