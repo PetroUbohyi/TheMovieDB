0e31e82cdb55bc62dd4c9d477924c423de62ed7d
petro.ubohyi <petro.ubohyi@globallogic.com>
2022-01-24 15:38:14 +0200

    created rough view of movie details screen without Top Billed Cast

--- themoviedb/lib/presentation/screens/movie_details_screen.dart
+++ themoviedb/lib/presentation/screens/movie_details_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:themoviedb/cubit/movie_detail_cubit.dart';
 import 'package:themoviedb/data/api_client.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class MovieDetailsScreen extends StatelessWidget {
   final int movieId;
@@ -16,6 +17,7 @@ class MovieDetailsScreen extends StatelessWidget {
         builder: (context, state) {
       if (state is MovieDetailLoadingState) {
         return Scaffold(
+          backgroundColor: Colors.black,
           appBar: AppBar(
             title: Text('Loading...'),
           ),
@@ -26,8 +28,8 @@ class MovieDetailsScreen extends StatelessWidget {
       }
       if (state is MovieDetailLoadedState) {
         final movieDetails = (state as MovieDetailLoadedState).movieDetails;
-
         return Scaffold(
+          backgroundColor: Colors.black,
           appBar: AppBar(
             title: Text(movieDetails.title),
           ),
@@ -44,6 +46,17 @@ class MovieDetailsScreen extends StatelessWidget {
                   movieTitle: movieDetails.title,
                   releaseDate: movieDetails.releaseDate!,
                 ),
+              ),
+              _UserScoreWidget(
+                voteAverage: movieDetails.voteAverage,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                child: _TaglineWidget(tagLine: movieDetails.tagline),
+              ),
+              _OverviewWidget(
+                overview: movieDetails.overview!,
               ),
             ],
           ),
@@ -67,7 +80,7 @@ class _TopPosterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 370 / 190,
+      aspectRatio: 390.4 / 175.68 ,
       child: Stack(
         fit: StackFit.loose,
         children: [
@@ -99,7 +112,7 @@ class _MovieTitleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
         RichText(
           maxLines: 3,
@@ -109,15 +122,128 @@ class _MovieTitleWidget extends StatelessWidget {
               TextSpan(
                 text: movieTitle,
                 style: TextStyle(
-                    color: Colors.black,
+                    color: Colors.white,
                     fontSize: 20,
                     fontWeight: FontWeight.bold),
               ),
-              TextSpan(text: releaseDate)
+              TextSpan(
+                text: '($releaseDate)',
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              )
             ],
           ),
         ),
       ],
+    );
+  }
+}
+
+class _UserScoreWidget extends StatelessWidget {
+  final double voteAverage;
+
+  const _UserScoreWidget({Key? key, required this.voteAverage})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextButton(
+            onPressed: () {},
+            child: Row(
+              children: [
+                CircularPercentIndicator(
+                  radius: 22.0,
+                  lineWidth: 4.0,
+                  percent: voteAverage / 10,
+                  center: Text(
+                    (voteAverage * 10).toString(),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  progressColor: Colors.green,
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Text(
+                  'User Score',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 1,
+            height: 30,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Row(
+              children: [
+                Icon(
+                  Icons.play_arrow,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Text('Play Trailer', style: TextStyle(color: Colors.white),),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _TaglineWidget extends StatelessWidget {
+  final String? tagLine;
+
+  const _TaglineWidget({Key? key, required this.tagLine}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      tagLine!,
+      style: TextStyle(color: Colors.grey),
+    );
+  }
+}
+
+class _OverviewWidget extends StatelessWidget {
+  final String overview;
+
+  const _OverviewWidget({Key? key, required this.overview}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Overview',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            overview,
+            style: TextStyle(color: Colors.white),
+          )
+        ],
+      ),
     );
   }
 }
